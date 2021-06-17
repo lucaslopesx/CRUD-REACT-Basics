46075569e5ffd11110d880565014f0f5249fd167
Lucas Lopes <68665689+lucaslopesx@users.noreply.github.com>
2021-06-17 13:39:32 -0300

    COMMIT todo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,13 @@
+import Todo from "./components/Todo";
 function App() {
-    return <div>Hello!</div>;
+    return (
+        <div>
+            <h1>TODO</h1>
+            <Todo text="Learn React" />
+            <Todo text="Master React" />
+            <Todo text="Explore the full React Course" />
+        </div>
+    );
 }
 
 export default App;
